524ee525252f7be6926ec8822c139d8b09df0b22
dadam88 <derekadam1988@gmail.com>
2016-03-18 04:02:28 -0500

    fixin venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/derek/pianoProject/venv"
+set -gx VIRTUAL_ENV "/home/derek/djangoMusic/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/derek/pianoProject/venv/bin/python
+#!/home/derek/djangoMusic/venv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
